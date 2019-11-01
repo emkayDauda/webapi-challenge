071402969d5c3a7cfccbf448be5c17d7a6471822
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-01 10:41:06 +0100

    :sparkles: Create endpoint for posting new action
    Create action body validator as well

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -21,6 +21,24 @@ actionRouter.get('/:id', actionIdValidator, (req, res) => {
     res.status(201).json(req.valAction)
 })
 
+actionRouter.post('/', [projectIdValidator, actionBodyValidator], (req, res) => {
+    actionsDbHelper.insert({
+        "project_id": req.valProject.id,
+        ...req.valActionBody
+    })
+    .then(createdAction => res.status(201).json({
+        error: false,
+        message: "Success",
+        data: createdAction
+    }))
+    .catch(err =>
+        res.status(500).json({
+          error: true,
+          message: err.message
+        })
+      );
+})
+
 function actionIdValidator(req, res, next) {
     const { id } = req.params;
     actionsDbHelper
@@ -43,8 +61,27 @@ function actionIdValidator(req, res, next) {
       );
 }
 
+function actionBodyValidator(req, res, next) {
+    const {project_id, description, notes} = req.body;
+
+    if(!Object.keys(req.body).length){
+        res.status(400).json({
+            error: true,
+            message: "Request body missing"
+        })
+    } else if (!project_id || !description || !notes) {
+        res.status(400).json({
+            error: true,
+            message: "project_id, description and notes are required fields"
+        })
+    } else {
+        req.valActionBody = {project_id, description, notes}
+        next()
+    }
+}
+
 function projectIdValidator(req, res, next) {
-  const { id } = req.params;
+  const id = req.params.id || req.body.project_id;
   projectsDbHelper
     .get(id)
     .then(project => {
