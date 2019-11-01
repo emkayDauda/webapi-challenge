74bf6a9eda28ec3ecb6442e26ac1e1dd5d7c599a
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-01 09:27:09 +0100

    :sparkles: Create get endpoint for projects

---
+++ routers/projectsRouter.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const projectsModel = require('../data/helpers/projectModel')
+const project = express();
+
+project.get('/', (req, res) => {
+    projectsModel.get()
+    .then(projects => {
+        res.status(200).json(projects)
+    })
+    .catch(err => res.status(500).json({
+        error: err.message,
+        message: 'An error occurred while fetching from database'
+    }))
+})
